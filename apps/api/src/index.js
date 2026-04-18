8f16424b10a8e9d5a9c23f42c2c16bffb70b09a7
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-04-18 15:38:50 +0500

    feat: implement dummy signup

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -2,6 +2,24 @@ import Fastify from "fastify"
 
 const fastify = Fastify()
 
+const users = new Map()
+
 fastify.get( "/", () => ( { message: "ok" } ) )
+
+fastify.post( "/join", ( req, res ) => {
+
+	const { username, email, password } = req.body
+
+	if ( users.has( username ) ) {
+
+		return res.status( 400 ).send( {
+			code: "APP_AUTH_USERNAME_EXISTS",
+		} )
+	}
+
+	users.set( username, { email, password } )
+
+	return { username, email }
+} )
 
 fastify.listen( { port: 3_000 } )

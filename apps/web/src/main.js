7001033667d72cf4cf42f2c3a5307df4881ed131
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-04-18 17:13:02 +0500

    implement basic router

--- apps/web/src/main.js
+++ apps/web/src/main.js
@@ -1,37 +1,133 @@
 import "./main.css"
 
-const username = document.getElementById( "username" )
-const password = document.getElementById( "password" )
-const email = document.getElementById( "email" )
-const form = document.querySelector( "form" )
+const app = document.getElementById( "app" )
 
-form.onsubmit = async e => {
+checkRoutes()
 
-	e.preventDefault()
+function checkRoutes() {
 
-	const body = {
-		username: username.value,
-		password: password.value,
-		email: email.value,
+	if ( window.location.pathname === "/" ) {
+
+		if ( !isLoggedIn() ) {
+
+			return ( window.location.pathname = "/join" )
+		}
+
+		homePage()
 	}
+	else if ( window.location.pathname === "/join" ) {
 
-	console.log( body )
+		if ( isLoggedIn() ) {
 
-	try {
-		const response = await fetch( "http://localhost:3000/join", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify( body ),
-		} )
+			return ( window.location.pathname = "/" )
+		}
 
-		const json = await response.json()
-
-		console.log( json )
+		joinPage()
 	}
-	catch( error ) {
+}
 
-		console.error( error )
+function homePage() {
+
+	const user = getUser()
+
+	const innerHTML =  `
+		<h1>Hi, @${ user.username }!</h1>
+		<button id="logout">Logout</button>
+	`
+
+	app.innerHTML = innerHTML
+
+	const logoutButton = document.getElementById( "logout" )
+
+	logoutButton.onclick = () => {
+
+		if ( logout() ) {
+
+			window.location.pathname = "/"
+		}
 	}
+}
+
+function joinPage() {
+
+	const innerHTML =  `
+		<form class="auth">
+			<div>
+				<input id="username" type="text" autocomplete="username" placeholder="Username" required>
+			</div>
+			<div>
+				<input id="email" type="email" placeholder="Email" />
+			</div>
+			<div>
+				<input id="password" type="password" autocomplete="new-password" placeholder="Password" required>
+			</div>
+			<div>
+				<button>Signup</button>
+			</div>
+			<div>
+				<a href='/login'>Login</a>
+			</div>
+		</form>
+	`
+
+	app.innerHTML = innerHTML
+
+	//
+
+	const username = document.getElementById( "username" )
+	const password = document.getElementById( "password" )
+	const email = document.getElementById( "email" )
+	const form = document.querySelector( "form" )
+
+	//
+
+	form.onsubmit = async e => {
+
+		e.preventDefault()
+
+		const body = {
+			username: username.value,
+			password: password.value,
+			email: email.value,
+		}
+
+		try {
+			const response = await fetch( "http://localhost:3000/join", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify( body ),
+			} )
+
+			const json = await response.json()
+
+			localStorage.setItem( "auth_token", json.token )
+			localStorage.setItem( "user", JSON.stringify( { username: json.username } ) )
+
+			window.location.pathname = "/"
+		}
+		catch( error ) {
+
+			console.error( error )
+		}
+	}
+}
+
+function isLoggedIn() {
+
+	return !!localStorage.getItem( "auth_token" )
+}
+
+function getUser() {
+
+	return JSON.parse( localStorage.getItem( "user" ) || "{}" )
+}
+
+function logout() {
+
+	localStorage.removeItem( "auth_token" )
+	localStorage.removeItem( "user" )
+
+	return true
 }
